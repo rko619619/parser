f29618d1ec5ba2885b269857f167a9315a28653b
Yan Ramanovich <Yan_Ramanovich@epam.com>
2021-03-22 11:55:26 +0300

    Refactoring

--- parser.rb
+++ parser.rb
@@ -1,104 +1,110 @@
-class CorrectArgument
-  attr_accessor :file_path
+class Validator
+  attr_reader :file_path
+
+  EXPAN = ['.log']
 
   def initialize(file_path)
     @file_path = file_path
   end
 
   def check
-    if !@file_path.nil?
-      puts('A file is empty')
-      if (File.extname(@file_path) == '.log') && File.exist?(@file_path)
-        puts('A good file to work')
-        true
-      else
-        false
-      end
+    if nil_file && extname && exist
+      true
     else
       puts('Can you enter a correct file?')
       false
     end
   end
+
+  private
+
+  def nil_file
+    !file_path.nil?
+  end
+
+  def exist
+    File.exist?(file_path)
+  end
+
+  def extname
+    EXPAN.include?(File.extname(file_path))
+  end
 end
 
-class ReadFile
+
+class Reader
+  attr_reader :file_path
+
   def initialize(file_path)
     @file_path = file_path
   end
 
   def read
-    File.read(@file_path).split(/\n+/)
+    File.readlines(file_path)
   end
 end
 
-class SplitData
+class Spliter
+  attr_reader :file_read, :data
   def initialize(file_read)
     @file_read = file_read
     @data = Hash.new([])
   end
 
   def correct_data
-    @file_read.map { |x| x.split(' ') }.each do |var|
-      @data[var[0]] += [var[1]]
+    file_read.map { |x| x.split(' ') }.each do |var|
+      data[var[0]] += [var[1]]
     end
-    @data
+    data
   end
 end
 
-class VisitsWebpage
+class VisitsDecorator
+  attr_reader :correct_data
   def initialize(correct_data)
-    @correct_hash = Hash.new(0)
     @correct_data = correct_data
   end
 
   def correct
-    @correct_data.each do |key, value|
-      @correct_hash[key] += value.length
-    end
-    sort(@correct_hash)
+    visits = correct_data.sort_by { |key, value| value.length }.reverse.to_h
+    write(visits)
   end
 
   private
 
-  def sort(hash)
-    sorted_hash = hash.sort_by { |_key, value| value }.reverse.to_h
-    write(sorted_hash)
-  end
-
   def write(sorted_hash)
-    puts(sorted_hash.map { |key, value| "#{key} #{value} visits " }.join("\n"))
+    puts(sorted_hash.map { |key, value| "#{key} #{value.length} visits " }.join("\n"))
   end
 end
 
-class UniqVisits
+class UniqVisitsDecorator
+  attr_reader :correct_data
   def initialize(correct_data)
-    @correct_hash = Hash.new(0)
     @correct_data = correct_data
   end
 
   def correct
-    @correct_data.each do |key, value|
-      @correct_hash[key] += value.uniq.length
-    end
-    write(@correct_hash)
+    uniq_visits = correct_data.sort_by { |key, value| value.uniq.length }.reverse.to_h
+    write(uniq_visits)
   end
 
   private
 
   def write(correct_hash)
-    puts(correct_hash.map { |k, v| "#{k} #{v} unique views" })
+    puts(correct_hash.map { |key, value| "#{key} #{value.uniq.length} unique views" })
   end
 end
 
+
 ARGV.each do |file_path|
-  argv = CorrectArgument.new(file_path)
+  argv = Validator.new(file_path)
   next unless argv.check
-  file = ReadFile.new(file_path)
+  file = Reader.new(file_path)
   file_read = file.read
-  data = SplitData.new(file_read)
+  data = Spliter.new(file_read)
   correct_data = data.correct_data
-  visits = VisitsWebpage.new(correct_data)
+  visits = VisitsDecorator.new(correct_data)
   visits.correct
-  uniq_visits = UniqVisits.new(correct_data)
+  uniq_visits = UniqVisitsDecorator.new(correct_data)
   uniq_visits.correct
 end
